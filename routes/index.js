318859972cb2d0bc3660d3f4ac37987f8e60d9e7
vaishnav98 <mavaishnav007@gmail.com>
2018-12-31 04:27:39 +0530

    tidy up

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,14 @@ var express = require('express');
 var router = express.Router();
 var db = require('../queries');
 
-
-
 router.get('/api/users', db.getAllUsers);
 router.post('/api/users', db.createUser);
 router.get('/api/devices', db.getAllDevices);
 router.get('/api/devices/:user', db.getSingleDevice);
 router.post('/api/devices', db.createDevice); 
-// application -------------------------------------------------------------
 router.get('/', function (req, res) {
 
-    res.render('index', {title: 'mqtt-mosquitto-auth-backend'}); // load the single view file (angular will handle the page changes on the front-end)
+    res.render('index', {title: 'mqtt-mosquitto-auth-backend'}); 
 });
 
 module.exports = router;
